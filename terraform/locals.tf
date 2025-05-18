ffd5316157303ea965dfc30fa99ec85ac4ce9b9e
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-05-18 19:31:16 +0200

    feat: kubeai

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,12 +1,14 @@
 locals {
   name         = "ai-${var.name}-cluster"
+  eks_name     = "${local.name}-eks"
   azs          = var.azs
   argocdDomain = "${local.name}-argocd.${var.zone_name}"
   sg           = "${local.name}-sg"
+  karpenter_sg = "karpenter_sg-${local.sg}"
   azs_count = length(var.azs)
 
-  app_name  = "kivoyo"
-  namespace = local.app_name
+  app_name         = "kivoyo"
+  namespace        = local.app_name
   kubeai_namespace = var.kubeai_ns
 
   db_name = replace(local.name, "-", "_")
---
+++ terraform/sg.tf
@@ -0,0 +1,26 @@
+module "karpenter_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.karpenter_sg
+  description = "Security group for Karpenter"
+  vpc_id = module.vpc.vpc_id
+
+  # Recommended ingress rules
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "all-all"
+      source_security_group_id = module.eks.cluster_primary_security_group_id
+    }
+  ]
+
+  # Egress rules to allow node communication
+  egress_rules = ["all-all"]
+
+  tags = merge(
+    local.tags,
+    {
+      "karpenter.sh/discovery" = local.eks_name
+    }
+  )
+}
